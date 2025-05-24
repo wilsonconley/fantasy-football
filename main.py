be72e9903e04234481e28b5375e7b5f1529dd9b8
wilsonconley <conleywilson0@gmail.com>
2025-05-24 14:05:47 -0400

    Set up initial Selenium driver

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+from __future__ import annotations
+
+import contextlib
+import typing as t
+
+import selenium.webdriver.support.expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+@contextlib.contextmanager
+def selenium_driver() -> t.Iterator[WebDriver]:
+    """Context manager for a Selenium web scraper."""
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def ensure_clickable(
+    driver: WebDriver, by: str, identifier: str, timeout: float = 20.0
+) -> WebElement:
+    element = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable((by, identifier))
+    )
+    return element
+
+
 def main():
-    print("Hello from example!")
+    with selenium_driver() as driver:
+        driver.get("https://selenium.dev/documentation")
+        assert "Selenium" in driver.title
+
+        elem = driver.find_element(By.ID, "m-documentationwebdriver")
+        elem.click()
+        assert "WebDriver" in driver.title
 
 
 if __name__ == "__main__":
